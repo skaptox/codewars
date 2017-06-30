d1ff47f60efcc152f0d43e0bf1fe944b85a08e80
skaptox <oalbornoz08@gmail.com>
2017-06-30 18:27:03 -0400

    Added files

---
+++ 7Kyu/Ruby/Friend or Foe.rb
@@ -0,0 +1,15 @@
+=begin
+
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours!
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+=end
+
+def friend(friends)
+  friends.select { |x| x.length == 4}
+end
+
+
