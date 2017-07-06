7d19505eef7a3a9e7d8cde10eb1c2c838dae5bb1
skaptox <oalbornoz08@gmail.com>
2017-07-06 19:35:13 -0400

    Added files

---
+++ Ruby/5Kyu/Weight for weight.rb
@@ -0,0 +1,53 @@
+=begin 
+
+My friend John and I are members of the "Fat to Fit Club (FFC)". John is worried because each
+month a list with the weights of members is published and each month he is the last on the list
+which means he is the heaviest.
+
+I am the one who establishes the list so I told him: "Don't worry any more, I will modify the order
+of the list". It was decided to attribute a "weight" to numbers. The weight of a number will be from
+now on the sum of its digits.
+
+For example 99 will have "weight" 18, 100 will have "weight" 1 so in the list 100 will come before
+99. Given a string with the weights of FFC members in normal order can you give this string ordered
+by "weights" of these numbers? Example:
+
+"56 65 74 100 99 68 86 180 90 ordered by numbers weights becomes: 100 180 90 56 65 74 68 86 99
+
+When two numbers have the same "weight", let us class them as if they were strings and not numbers:
+100 is before 180 because its "weight" (1) is less than the one of 180 (9) and 180 is before 90
+since, having the same "weight" (9) it comes before as a string.
+
+All numbers in the list are positive numbers and the list can be empty. 
+
+=end
+
+def order_weight(strng)
+	weights = strng.split(" ") #Convert string to array 
+	digitsSum = weights.map{|x| x.chars.map(&:to_i).reduce(:+)} #Add the digits of each member
+	indices = (0...weights.size()).to_a
+	aHash = indices.zip(digitsSum).to_h #Make a hash with two arrays
+	aHash = aHash.sort_by{|k,v| v}.to_h #Sort the hash by digit sum
+
+	keys,values = aHash.keys, aHash.values 
+
+	result = []
+	max = weights.size()
+	i = 0
+	while i < max
+		pack = []
+		pack << weights[keys[i]] #The keys represent the order fake of the weights
+		while i + 1 < max and values[i] == values[i + 1]
+			pack << weights[keys[i+1]] #Accumulate weights with equal valor
+			i += 1
+		end
+		pack = pack.sort #Sort equal weights by string
+		result += pack
+		i += 1
+	end
+	result.join(" ")
+end
+
+
+puts order_weight("71899703 200 6 91 425 4 67407 7 96488 6 4 2 7 31064 9 7920 1 34608557 27 72 18 81") == 
+"1 2 200 4 4 6 6 7 7 18 27 72 81 9 91 425 31064 7920 67407 96488 34608557 71899703" #true
