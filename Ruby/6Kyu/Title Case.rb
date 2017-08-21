f5f2e48be8900133e8ed461802d96e57ac4c5be6
Oscar Albornoz <oalbornoz08@gmail.com>
2017-08-20 21:24:44 -0400

    Added files

---
+++ Ruby/6Kyu/Title Case.rb
@@ -0,0 +1,25 @@
+=begin
+	
+A string is considered to be in title case if each word in the string is either
+(a) capitalised (that is, only the first letter of the word is in upper case) or
+(b) considered to be an exception and put entirely into lower case unless it is
+the first word, which is always capitalised.
+
+Write a function that will convert a string into title case, given an optional
+list of exceptions (minor words). The list of minor words will be given as a
+string with each word separated by a space. Your function should ignore the case
+of the minor words string -- it should behave in the same way even if the case
+of the minor word string is changed.
+	
+=end
+
+
+
+def title_case(title, minor_words = '')
+  title.capitalize.split().map{|w| minor_words.downcase.split().include?(w) ? w : w.capitalize}.join(' ')
+end
+
+puts title_case('a clash of KINGS', 'a an the of') == 'A Clash of Kings'
+puts title_case('THE WIND IN THE WILLOWS', 'The In') == 'The Wind in the Willows'
+puts title_case('the quick brown fox') == 'The Quick Brown Fox'
+
